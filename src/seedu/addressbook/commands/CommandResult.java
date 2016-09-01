b384e2308cb3dc6274b0fecb34ec5b0c58b82634
acuodancer <leminhduc74@gmail.com>
2016-09-01 15:41:45 +0800

    Revert "Change feedbackToUser access modifier to private and add getter, change TestUI.java to use getter"
    This reverts commit d0e7f48a0311d20b847060705ab56746bf35c312.

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,13 +11,9 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    private final String feedbackToUser;
+    public final String feedbackToUser;
 
-    public String getFeedbackToUser() {
-		return feedbackToUser;
-	}
-
-	/** The list of persons that was produced by the command */
+    /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
     public CommandResult(String feedbackToUser) {
