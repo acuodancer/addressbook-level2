43446cdb4485366567fa8b1772b0c89862072363
acuodancer <leminhduc74@gmail.com>
2016-09-01 17:46:49 +0800

    Add unit and postal code to Address.EXAMPLE

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,7 +9,7 @@ import seedu.addressbook.address.*;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
+    public static final String EXAMPLE = "123, some street, #01-22, 1234567";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
