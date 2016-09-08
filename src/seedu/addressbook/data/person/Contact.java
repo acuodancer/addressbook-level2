6d7fdd531a8b53bb0119b0f7ff0327cfb2139cfb
acuodancer <leminhduc74@gmail.com>
2016-09-08 18:48:22 +0800

    Add setRegex method to Contact class to customize regex for each contact type

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -2,8 +2,14 @@ package seedu.addressbook.data.person;
 
 public class Contact {
 
-	public static boolean isValid(String testContact, String regex) {
+	public static String regex = "";
+	
+	public static boolean isValid(String testContact) {
 		return testContact.matches(regex);
+	}
+	
+	public static void setRegex(String newRegex) {
+		regex = newRegex;
 	}
 	
 }
