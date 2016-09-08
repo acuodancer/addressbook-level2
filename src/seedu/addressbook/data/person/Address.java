93e42e1205eaa26b33b92d5e3794de2b6dc90e54
acuodancer <leminhduc74@gmail.com>
2016-09-08 19:06:13 +0800

    Remove value, isPrivate field and isPrivate() method and use those of the super class instead

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -12,20 +12,16 @@ public class Address extends Contact {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
-    private boolean isPrivate;
-
-    /**
+     /**
      * Validates given address.
      *
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
-        this.isPrivate = isPrivate;
+    	super(address, isPrivate);
         if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        }
-        this.value = address;
+        } 
     }
 
     /**
@@ -51,8 +47,5 @@ public class Address extends Contact {
     public int hashCode() {
         return value.hashCode();
     }
-
-    public boolean isPrivate() {
-        return isPrivate;
-    }
+    
 }
